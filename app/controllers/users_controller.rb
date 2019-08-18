ff2548f0d19401038ce27e5c0c558cd99902a09e
su9arfree <tompham3385@gmail.com>
2019-08-18 14:19:38 +0700

    use container

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,17 +26,25 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+    if @user.save
+      log_in @user
+      flash[:success] = "welcome"
+      redirect_to @user
       else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        render 'new'
       end
     end
-  end
+
+  #   respond_to do |format|
+  #     if @user.save
+  #       format.html { redirect_to @user, notice: 'User was successfully created.' }
+  #       format.json { render :show, status: :created, location: @user }
+  #     else
+  #       format.html { render :new }
+  #       format.json { render json: @user.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'devices/index'
   get 'devices/new'
   get 'devices/show'
   get 'devices/edit'
   get 'pages/home'
   get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   root 'pages#home'
   resources :users
   resources :devices
